ee1bff102a52cfbb9afac4be245cbd2de3730213
YoItzChip <bryce10butler@gmail.com>
2025-09-21 00:20:24 -0700

    added a silly secret

--- musicplayer/script.js
+++ musicplayer/script.js
@@ -16,3 +16,16 @@ function toggleButton() {
     }
     isplaying = !isplaying
 }
+let text1 = document.getElementById("secret1")
+let catpng = document.getElementById("cat")
+const victorysfx = new Audio("../audios/victorysfx.mp3")
+let clicked = false
+
+function secret() {
+    if (!clicked) {
+        victorysfx.play()
+        text1.innerText = "congrats.. you uh.. you get a possum"
+        catpng.src = "../images/littlePossumGuy.jpg"
+    }
+    clicked = true
+}
